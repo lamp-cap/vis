de19c4a8fb3d6c9edf1c0acb3b310f5a4e5b48f1
lamp-cap <131742619+lamp-cap@users.noreply.github.com>
2023-09-18 10:32:51 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import counterReducer from './counterStore'
-import urlReducer from './imgUrl'
+import referImgReducer from './referImgUrl'
+import data1Reducer from './data1'
+import data2Reducer from './data2'
 
 export default configureStore({
   reducer: {
     // 注册子模块
-    counter: counterReducer,
-    url: urlReducer
+    counter: referImgReducer,
+    data1: data1Reducer,
+    data2: data2Reducer
   }
 })
