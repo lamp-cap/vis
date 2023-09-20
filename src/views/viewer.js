ef1b3861bdd409d3c45f899297e33fdf2a606ebf
lamp-cap <131742619+lamp-cap@users.noreply.github.com>
2023-09-20 10:57:21 +0800

    Update viewer.js

--- src/views/viewer.js
+++ src/views/viewer.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector} from 'react-redux';
 import Img from './image';
 import Img1 from './neighbor/img5.png';
 
 const Index = (props) => {
-  const { imgUrl } = useSelector(state => state.counter);
-  var url = imgUrl;
+  const { dataList1 } = useSelector(state => state.data1);
+  const [ url, changeUrl ] = useState(Img1);
+  const imgPath = require.context('./images/', true, /\.png$/);
+  const paths = imgPath.keys();
+
   const img1Ref = React.useRef(null);
   const img2Ref = React.useRef(null);
   const [drag, switchState] = useState(true);
@@ -24,10 +27,17 @@ const Index = (props) => {
     img1Ref.current.restore();
     img2Ref.current.restore();
   }
+  useEffect(()=>{
+    for(var i=0;i<paths.length;i++)
+    {
+      if(paths[i].search(dataList1) !== -1)
+      changeUrl(imgPath(paths[i]))
+    }
+  },[dataList1, imgPath, paths])
   return (
     <>
-      <div style={{display:"flex",justifyContent:"flex-end"}}>
-        <div style={{height:"66vh", width:"5vw", backgroundColor:"#666", display:"flex", flexDirection:"column",justifyContent:"space-around",alignItems:"center",color:"white"}}>
+      <div style={{display:"flex"}}>
+        <div style={{height:"66vh", width:"4.5vw", backgroundColor:"#666", display:"flex", flexDirection:"column",justifyContent:"space-around",alignItems:"center",color:"white"}}>
           <div style={{height:"60%",display:"flex", flexDirection:"column",justifyContent:"space-around"}}>
             <div style={{height:"3vw",width:"3vw",borderRadius:"5px",border: "2px solid #fff"}} onClick={mswitch}>{drag?"Drag":"Select"}</div>
             <div style={{height:"3vw",width:"3vw",borderRadius:"5px",border: "2px solid #fff"}} onClick={restore}>重置</div>
@@ -35,13 +45,13 @@ const Index = (props) => {
           </div>
           <div>
             <div style={{height:"3vw",width:"3vw",borderRadius:"5px",border: "2px solid #fff"}}>
-              <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png" multiple />
+              <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png" />
             </div>
           </div>
         </div>
-        <div style={{columnCount:2, width:"45vw",height:"66vh", backgroundColor:"#fff"}}>
+        <div style={{columnCount:2, width:"44vw",height:"66vh", backgroundColor:"#fff", columnGap:"1px"}}>
           <Img url={Img1} title={"title1"} ref={img1Ref} drag={drag} cursor={cursor} setCursor={setCursor}/>
-          <Img url={url}  title={"title2"} ref={img2Ref} drag={drag} cursor={cursor} setCursor={setCursor}/>
+          <Img url={url}  title={"title2"} ref={img2Ref} drag={drag} cursor={cursor} setCursor={setCursor} id={dataList1}/>
         </div>
       </div>
       
